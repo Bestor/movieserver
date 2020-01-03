65946a1d213ba050a2ed07cfc7da4a4bf94e083b
Josh Dale <joshua.dou.dale@hpe.com>
2020-01-03 10:55:48 -0700

    Set up django rest framework

--- movieserver/movieserver/urls.py
+++ movieserver/movieserver/urls.py
@@ -1,9 +1,24 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from movieserver.resources import MovieResource
-movie_resource = MovieResource()
+from models import Movies
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class MovieSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Movies
+        fields = ['title', 'format', 'length', 'year', 'rating']
+
+# ViewSets define the view behavior.
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movies.objects.all()
+    serializer_class = MovieSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'movies', MovieViewSet)
 
 urlpatterns = [
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(movie_resource.urls)),
 ]
